2f13fc286d2eafc6b707365d9875406c9bdc3d14
WADA_12 <WADA_12@DESKTOP-44FIDIA>
2024-04-23 16:42:45 -0700

    Add project files.

---
+++ WebApplication1/Models/FacultyStudentContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class FacultyStudentContext : DbContext 
+    {
+        public FacultyStudentContext(DbContextOptions<FacultyStudentContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Faculty> Faculties { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
---
+++ WebApplication1/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Student
+    {
+
+        public int ID { get; set; }
+        [Required]
+        public string StudentName { get; set; }
+        public string Address { get; set; }
+        [ForeignKey("Faculty")]
+        public int FacultyID { get; set; }
+        [ValidateNever]
+        public virtual Faculty Faculty { get; set; }
+
+    }
+}
